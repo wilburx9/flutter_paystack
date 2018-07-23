a5237f82826b937eca96ed9915adaa69e547c99c
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2018-07-23 15:40:16 +0100

    Done with PaymentCard Object

---
+++ lib/utils/card_utils.dart
@@ -0,0 +1,53 @@
+class CardUtils {
+  static bool isWholeNumberPositive(String value) {
+    if(value == null) {
+      return false;
+    }
+
+    for (var i = 0; i < value.length; ++i) {
+      if(!((value.codeUnitAt(i) ^ 0x30) <= 9)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  static bool hasMonthPassed(int year, int month) {
+    var now = DateTime.now();
+    // The month has passed if:
+    // 1. The year is in the past. In that case, we just assume that the month
+    // has passed
+    // 2. Card's month (plus another month) is more than current month.
+    return hasYearPassed(year) ||
+        convertYearTo4Digits(year) == now.year && (month < now.month + 1);
+  }
+
+  static bool isValidMonth(int month){
+    return (month > 0) && (month < 13);
+  }
+
+  static bool hasYearPassed(int year) {
+    int fourDigitsYear = convertYearTo4Digits(year);
+    var now = DateTime.now();
+    // The year has passed if the year we are currently is more than card's
+    // year
+    return fourDigitsYear < now.year;
+  }
+
+  static bool isNotExpired(int year, int month) {
+    // It has not expired if both the year and date has not passed
+    return !hasYearPassed(year) && !hasMonthPassed(year, month);
+  }
+
+  /// Convert the two-digit year to four-digit year if necessary
+  static int convertYearTo4Digits(int year) {
+    if (year < 100 && year >= 0) {
+      var now = DateTime.now();
+      String currentYear = now.year.toString();
+      String prefix = currentYear.substring(0, currentYear.length - 2);
+      year = int.parse('$prefix${year.toString().padLeft(2, '0')}');
+    }
+    return year;
+  }
+}
---
+++ lib/utils/string_utils.dart
@@ -0,0 +1,5 @@
+class StringUtils {
+  static bool isEmpty(String value){
+    return value == null || value.length < 1 || value.toLowerCase == "null";
+  }
+}
