5e03cb511a6d37be2132749cd9ad9a1bd5ab258e
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2018-07-23 17:45:28 +0100

    Done with Charge Object

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,5 +1,18 @@
 class StringUtils {
-  static bool isEmpty(String value){
+  static bool isEmpty(String value) {
     return value == null || value.length < 1 || value.toLowerCase == "null";
+  }
+
+  static bool isValidEmail(String email) {
+    String p =
+        r"[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+";
+    RegExp regExp = new RegExp(p);
+    return regExp.hasMatch(email);
   }
 }
