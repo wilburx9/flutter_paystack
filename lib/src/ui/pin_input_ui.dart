8b5db6ada4d059e2594bb53179f2f9cb5651817f
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2018-07-27 10:39:45 +0100

    Chnaged all Flutter prompts dialogs

--- lib/src/ui/pin_input_ui.dart
+++ lib/src/ui/pin_input_ui.dart
@@ -2,26 +2,21 @@ import 'package:flutter/material.dart';
 
 class PinInputUI extends StatefulWidget {
   final bool randomize;
-  final String title;
   final int pinLength;
-  final String subHeading;
   final bool showIndicatorPlaceholder;
   final double indicatorPadding;
 
   PinInputUI(
       {@required this.randomize,
-      @required this.title,
-      @required this.pinLength,
-      @required this.subHeading,
-      @required this.showIndicatorPlaceholder,
-      @required this.indicatorPadding});
+        @required this.pinLength,
+        @required this.showIndicatorPlaceholder,
+        @required this.indicatorPadding});
 
   @override
   State<StatefulWidget> createState() => new _PinInputUIState();
 }
 
 class _PinInputUIState extends State<PinInputUI> {
-  var _scaffoldKey = new GlobalKey<ScaffoldState>();
   var _numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   List<String> _selectedPins = [];
 
@@ -39,56 +34,50 @@ class _PinInputUIState extends State<PinInputUI> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      key: _scaffoldKey,
-      appBar: new AppBar(
-        backgroundColor: Colors.lightBlue[900],
-        title: new Text(widget.title),
-      ),
-      body: new Container(
-        child: new Stack(
+    List<Widget> buttons = _getButtonsChildren();
+    return new Container(
+      child: SingleChildScrollView(
+        child: new ListBody(
           children: <Widget>[
-            new Positioned(
-                top: 20.0,
-                left: 10.0,
-                right: 10.0,
-                height: MediaQuery.of(context).size.height / 3.0,
-                child: new Container(
-                  alignment: Alignment.bottomCenter,
-                  child: new Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      new Text(
-                        widget.subHeading,
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(fontSize: 20.0),
-                      ),
-                      new SizedBox(height: 80.0),
-                      new Container(
-                          height: 20.0,
-                          alignment: Alignment.center,
-                          child: _getIndicators()),
-                      new SizedBox(
-                        height: 20.0,
-                      ),
-                    ],
-                  ),
-                )),
-            new Positioned(
-              bottom: 5.0,
-              left: 20.0,
-              right: 20.0,
-              height: MediaQuery.of(context).size.height / 2.0,
-              child: new Container(
-                child: new GridView.count(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1.5,
-                  physics: new NeverScrollableScrollPhysics(),
-                  padding: const EdgeInsets.all(4.0),
-                  children: _getButtonsChildren(),
-                ),
-              ),
-            )
+            new SizedBox(
+              height: 10.0,
+            ),
+            new Container(
+                height: 20.0,
+                alignment: Alignment.center,
+                child: _getIndicators()),
+            new SizedBox(
+              height: 20.0,
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: buttons.sublist(0, 3),
+            ),
+            new SizedBox(
+              height: 20.0,
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: buttons.sublist(3, 6),
+            ),
+            new SizedBox(
+              height: 20.0,
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: buttons.sublist(6, 9),
+            ),
+            new SizedBox(
+              height: 20.0,
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Expanded(child: buttons[9]),
+                new Expanded(child: buttons[10]),
+                new Expanded(child: buttons[11])
+              ],
+            ),
           ],
         ),
       ),
@@ -101,18 +90,23 @@ class _PinInputUIState extends State<PinInputUI> {
       var number = _numbers[i];
 
       if (number == 100) {
-        widgets.add(new IconButton(
-            icon: new Icon(Icons.backspace),
-            onPressed: _selectedPins.isNotEmpty ? _clearPins : null));
+        widgets.add(new Container(
+          alignment: Alignment.centerLeft,
+          child: new IconButton(
+              icon: new Icon(Icons.backspace),
+              onPressed: _selectedPins.isNotEmpty ? _clearPins : null),
+        ));
       } else if (number == 200) {
         widgets.add(new IconButton(
             onPressed: _selectedPins.length >= 4 ? _processSubmit : null,
             icon: new Text(
               'DONE',
+              textAlign: TextAlign.end,
+              softWrap: false,
               style: new TextStyle(
                   fontWeight: FontWeight.bold,
                   color:
-                      _selectedPins.length >= 4 ? Colors.black : Colors.grey),
+                  _selectedPins.length >= 4 ? Colors.black : Colors.grey),
             )));
       } else {
         widgets.add(getNumberWidget(number));
@@ -168,7 +162,7 @@ class _PinInputUIState extends State<PinInputUI> {
       for (var i = 0; i < placeHoldersLength; i++) {
         placeHolders.add(Padding(
           padding:
-              new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
+          new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
           child: new Icon(
             Icons.panorama_fish_eye,
             size: 17.0,
@@ -180,7 +174,7 @@ class _PinInputUIState extends State<PinInputUI> {
       List<Widget> indicators = _selectedPins.map((s) {
         return Padding(
           padding:
-              new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
+          new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
           child: new Icon(
             Icons.brightness_1,
             size: 17.0,
@@ -201,10 +195,10 @@ class _PinInputUIState extends State<PinInputUI> {
         children: _selectedPins.map((s) {
           return new Padding(
             padding:
-                new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
+            new EdgeInsets.symmetric(horizontal: widget.indicatorPadding),
             child: new Icon(
               Icons.brightness_1,
-              size: 17.0,
+              size: 10.0,
               color: Colors.grey,
             ),
           );
