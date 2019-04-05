8f2e5ed4839185fc9639d3c72235f1ccdb77307b
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2019-04-05 03:30:48 +0100

    Improved month input formatter

--- lib/src/widgets/common/input_formatters.dart
+++ lib/src/widgets/common/input_formatters.dart
@@ -2,28 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CardMonthInputFormatter extends TextInputFormatter {
+  String previousText;
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    var newText = newValue.text;
+    var text = newValue.text;
 
     if (newValue.selection.baseOffset == 0) {
       return newValue;
     }
 
     var buffer = new StringBuffer();
-    for (int i = 0; i < newText.length; i++) {
-      buffer.write(newText[i]);
+    for (int i = 0; i < text.length; i++) {
+      buffer.write(text[i]);
       var nonZeroIndex = i + 1;
-      if (nonZeroIndex % 2 == 0 && nonZeroIndex != newText.length) {
+
+      if (nonZeroIndex % 2 == 0 &&
+          ((!_isDeletion(previousText, text) && nonZeroIndex != 4) ||
+              (nonZeroIndex != text.length))) {
         buffer.write('/');
       }
     }
 
+    previousText = text;
     var string = buffer.toString();
     return newValue.copyWith(
-        text: string,
-        selection: new TextSelection.collapsed(offset: string.length));
+        text: string, selection: new TextSelection.collapsed(offset: string.length));
   }
 }
 
@@ -51,4 +55,12 @@ class CardNumberInputFormatter extends TextInputFormatter {
         text: string,
         selection: new TextSelection.collapsed(offset: string.length));
   }
+}
+
+bool _isDeletion(String prevText, String newText) {
+  if (prevText == null) {
+    return false;
+  }
+
+  return prevText.length > newText.length;
 }
