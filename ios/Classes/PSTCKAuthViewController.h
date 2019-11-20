41c434e40c4a302ce187032bc34e60e876ead80d
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2019-11-20 20:33:13 +0100

    Switched deprecated UIWebView for WKWebView

--- ios/Classes/PSTCKAuthViewController.h
+++ ios/Classes/PSTCKAuthViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 typedef void(^PSTCKAuthCallback)(void);
 
--- ios/Classes/PSTCKAuthViewController.m
+++ ios/Classes/PSTCKAuthViewController.m
@@ -10,13 +10,13 @@
 
 @interface PSTCKAuthViewController ()
 
-@property(nonatomic, strong) UIWebView *authenticationWebView;
+@property(nonatomic, strong) WKWebView *authenticationWebView;
 @property(nonatomic, copy) PSTCKAuthCallback completion;
 @property(nonatomic, strong) NSURL *authURL;
 
 @end
 
-@interface PSTCKAuthViewController (UIWebViewDelegate) <UIWebViewDelegate>
+@interface PSTCKAuthViewController (WKNavigationDelegate) <WKNavigationDelegate, WKUIDelegate>
 
 @end
 
@@ -42,10 +42,17 @@ BOOL handlingRedirectURL;
     
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(tappedCancelButton:)];
     self.navigationItem.leftBarButtonItem = cancelButton;
+    // Adds javascript to make content width the device width.
+    NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);";
+    WKUserScript *wkUScript = [[WKUserScript alloc] initWithSource:jScript injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+    WKUserContentController *wkUController = [[WKUserContentController alloc] init];
+    [wkUController addUserScript:wkUScript];
+    WKWebViewConfiguration *wkWebConfig = [[WKWebViewConfiguration alloc] init];
+    wkWebConfig.userContentController = wkUController;
     
-    self.authenticationWebView = [[UIWebView alloc] init];
-    self.authenticationWebView.delegate = self;
-    self.authenticationWebView.scalesPageToFit = NO;
+    self.authenticationWebView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:wkWebConfig];
+    self.authenticationWebView.UIDelegate = self;
+    self.authenticationWebView.navigationDelegate = self;
     [self.view addSubview:self.authenticationWebView];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
     self.navigationController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
@@ -71,11 +78,12 @@ BOOL handlingRedirectURL;
 
 @end
 
-@implementation PSTCKAuthViewController (UIWebViewDelegate)
+@implementation PSTCKAuthViewController (WKNavigationDelegate)
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-    #pragma unused(webView, navigationType)
-    NSString *url = [[request URL] absoluteString];
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+     #pragma unused(webView, navigationAction)
+    NSURLRequest *request = navigationAction.request;
+    NSString *url = [[request URL]absoluteString];
     
     // Prevent loading URL if it is the redirectURL
     // The intention is to only requery 3DS auths
@@ -84,18 +92,21 @@ BOOL handlingRedirectURL;
     // Processing has finished?
     if (handlingRedirectURL) {
         self.completion();
+        return decisionHandler(WKNavigationActionPolicyCancel);
+
     }
-    
-    return !handlingRedirectURL;
+    else {
+        return decisionHandler(WKNavigationActionPolicyAllow);
+    }
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    #pragma unused(webView)
-    
-    NSLog(@"%@", error);
-    NSString * myString = error.description;
-    NSLog(@"%@", myString);
-    
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    #pragma unused(webView, navigation)
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    #pragma unused(webView, navigation)
     // Turn off network activity indicator upon failure to load web view
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     
@@ -108,14 +119,6 @@ BOOL handlingRedirectURL;
     if (!handlingRedirectURL) {
         self.completion();
     }
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    #pragma unused(webView)
-
-    // Turn off network activity indicator upon finishing web view load
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-
 }
 
 @end
