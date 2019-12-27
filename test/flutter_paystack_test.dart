6d7ad0752f0c363e906ea76441b3ff65408f13ec
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2019-12-27 03:25:35 +0100

    Wrote basic test

---
+++ test/flutter_paystack_test.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_paystack/src/common/paystack.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_paystack');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  group("$PaystackPlugin", () {
+    test('is properly initialized with passed key', () async {
+      var publicKey = Platform.environment["PAYSTACK_TEST_PUBLIC_KEY"];
+      await PaystackPlugin.initialize(publicKey: publicKey);
+      expect(publicKey, PaystackPlugin.publicKey);
+    });
+  });
+}
