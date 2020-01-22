7c76dee4438aefbdda315de131c90835389bcc84
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2020-01-22 03:33:52 +0100

    Changes for 1.3.0

--- lib/src/api/service/base_service.dart
+++ lib/src/api/service/base_service.dart
@@ -11,7 +11,7 @@ mixin BaseApiService {
     'X-Paystack-Build': PlatformInfo().paystackBuild,
     'X-PAYSTACK-USER-AGENT':
         jsonEncode({'lang': Platform.isIOS ? 'objective-c' : 'kotlin'}),
-    'bindings_version': "1.0.2+1", // TODO: Update for every new versions
+    'bindings_version': "1.3.0", // TODO: Update for every new versions
     'X-FLUTTER-USER-AGENT': jsonEncode({'version': '1.0.0'})
   };
   final String baseUrl = 'https://standard.paystack.co';
