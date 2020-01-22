66acd0dffd0f407d50c48eb436cd34febb7edfe2
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2020-01-22 02:53:53 +0100

    Hard-coded bindings_version

--- lib/src/api/service/base_service.dart
+++ lib/src/api/service/base_service.dart
@@ -11,9 +11,7 @@ mixin BaseApiService {
     'X-Paystack-Build': PlatformInfo().paystackBuild,
     'X-PAYSTACK-USER-AGENT':
         jsonEncode({'lang': Platform.isIOS ? 'objective-c' : 'kotlin'}),
-    'bindings_version': Platform.isIOS
-        ? '3.0.5' // Latest version of Paystack official iOS SDK
-        : '3.0.10', // Latest version of Paystack official Android SDK
+    'bindings_version': "1.0.2+1", // TODO: Update for every new versions
     'X-FLUTTER-USER-AGENT': jsonEncode({'version': '1.0.0'})
   };
   final String baseUrl = 'https://standard.paystack.co';
